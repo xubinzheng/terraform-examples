933adb3c777ddc47ff4802e85321f4d40c63fcd4
barbieri <barbieri@us.ibm.com>
2018-01-09 16:10:59 -0500

    Create sample CAM template

---
+++ CAM/AWS/README.md
@@ -0,0 +1,2 @@
+# Sample Cloud Automation Manager Template
+Provision a single AWS instance
---
+++ CAM/AWS/main.tf
@@ -0,0 +1,26 @@
+#####################################################################
+##
+##      Created 10/31/17 by admin. For Cloud UCD-P@RS5
+##
+#####################################################################
+
+variable "aws_access_id" {}
+variable "aws_secret_key" {}
+variable "aws_instance_name" {}
+variable "key_name" {}
+
+provider "aws" {
+  access_key = "${var.aws_access_id}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+
+resource "aws_instance" "aws_instance" {
+  instance_type = "t2.micro"
+  ami = "ami-1d4e7a66"
+  key_name = "${var.key_name}"
+  tags {
+    Name = "${var.aws_instance_name}"
+  }
+}
