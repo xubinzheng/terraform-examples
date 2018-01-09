58ec577bc19dfc8958ccf9e44880183b923fa503
barbieri <barbieri@us.ibm.com>
2018-01-09 16:46:59 -0500

    Multi-file

--- CAM/AWS/main.tf
+++ CAM/AWS/main.tf
@@ -1,13 +1,3 @@
-#####################################################################
-##
-##      Created 10/31/17 by admin. For Cloud UCD-P@RS5
-##
-#####################################################################
-
-variable "aws_access_id" {}
-variable "aws_secret_key" {}
-variable "aws_instance_name" {}
-variable "key_name" {}
 
 provider "aws" {
   access_key = "${var.aws_access_id}"
---
+++ CAM/AWS/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_access_id" {}
+variable "aws_secret_key" {}
+variable "aws_instance_name" {}
+variable "key_name" {}
